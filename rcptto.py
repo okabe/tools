6971396bddf70856c6a13ca7170211384d6ba4c7
mpi <mikepicard01@gmail.com>
2016-05-11 10:52:19 -0400

    Update rcptto.py

--- rcptto.py
+++ rcptto.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python2
-#author: mp
-#comment: verify a list of emails using the RCPT TO method
-#you will probably have to change a lot of this
-#converation before running as some mail servers
-#dont follow the spec.
-
 import socket
 import sys
 
 sock  = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-sock.connect(( "smtp.example.com", 25 ))
+sock.connect(( sys.argv[1], 25 ))
         
 while True:
     data = sock.recv( 4096 )
+    print data
     if "ESMTP" in data: #data before sending EHLO
         sock.send( "EHLO lol.com\r\n" )
-    if "SMTPUTF8" in data: #last line after EHLO
+    if "250 CHUNKING" in data: #last line after EHLO
         sock.send( "MAIL FROM:<noone@gmail.com>\r\n" )
-    if "250 2.1.0 OK" in data:
-        for email in open( sys.argv[1], "r" ):
+    if "250" and "Sender OK" in data:
+        elem = 0
+        emails = [ x for x in open( sys.argv[2], "r" ) ]
+        for email in emails:
             """ check response for each email """
-            email = email.rstrip( "\n" )
-            sock.send( "RCPT TO:<{}>\r\n".format( email ) )
-            data = sock.recv( 4096 )
-            if "250" in data:
-                print "\033[1;32m[+]\033[1;m Valid: {}".format( email )
-            elif "550" in data:
-                print "\033[1;31m[!]\033[1;m Invalid: {}".format( email )
-            else:
-                print "\033[1;34[-]\033[1;m Fucked up"
+            while True:
+                email = emails[elem].rstrip( "\n" ) if elem < len( emails ) else None
+                if email is None:
+                    break
+                sock.send( "RCPT TO:<{}>\r\n".format( email ) )
+                data = sock.recv( 4096 )
+                if data is not None:
+                    #print data
+                    if "250" in data:
+                        print "\033[1;32m[+]\033[1;m Valid: {}".format( email )
+                    elif "550" in data:
+                        print "\033[1;31m[!]\033[1;m Invalid: {}".format( email )
+                    else:
+                        pass
+                elem += 1
+                data = None
         break
 
 sock.close()
+
