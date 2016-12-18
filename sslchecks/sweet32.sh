22f4378784d82c574a3943b8a93c92df2874ec94
okabé <mikepicard01@gmail.com>
2016-12-18 10:25:50 -0500

    Create sweet32.sh

---
+++ sslchecks/sweet32.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./openssl ciphers -v 'ALL:eNULL' \
+    | tr ':' '\n' \
+    | grep '3DES' \
+    | while read line ; do
+        c=$( echo $line | awk '{print $1}' )
+        (echo -en "GET / HTTP/1.0\r\n\r\n" ; sleep 1) \
+            | ./openssl s_client -connect $1 \
+                -cipher $c 2>/dev/null >/dev/null \
+                && echo "[+] Supports: $line" \
+                || echo "[!] Unsupported: $line" ;
+      done
