d97cd3f1ca9105a7b7b7d462bf5b72516fc3aea4
okabé <mikepicard01@gmail.com>
2016-12-18 10:55:24 -0500

    Update barmitzvah.sh

--- transport/barmitzvah.sh
+++ transport/barmitzvah.sh
@@ -5,8 +5,9 @@
     | grep RC4 \
     | while read line ; do
         c=$(echo $line | awk '{print $1}') ;
-        (echo -en "GET / HTTP/1.0\r\n\r\n" ; sleep 1) | ./openssl s_client -connect $1 \
-            -cipher $c 2>/dev/null >/dev/null \
-            && echo "[+] Supports: $line" \
-            || echo "[!] Unsupported: $line" ;
+        (echo -en "GET / HTTP/1.0\r\n\r\n" ; sleep 1) \
+            | ./openssl s_client -connect $1 \
+                -cipher $c 2>/dev/null >/dev/null \
+                && echo "[+] Supports: $line" \
+                || echo "[!] Unsupported: $line" ;
       done
