46a8aa3a157706f6747c3d8e61f0b9700e9e41c9
okabé <mikepicard01@gmail.com>
2016-12-18 10:27:04 -0500

    Create sharc.sh

---
+++ sslchecks/sharc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+stub=$1
+host=$( echo $stub | cut -f1 -d: )
+port=$( echo $stub | cut -f2 -d: )
+
+echo "[+] The following weak algorithms are supported:"
+ssh -p $port -o BatchMode=yes -vv test@$host 2>&1 \
+    | grep "kex_parse_kexinit" \
+    | cut -f3 -d: \
+    | tr ',' '\n' \
+    | grep arcfour \
+    | sort -u \
+    | while read line ; do
+        echo " $line"
+    done
