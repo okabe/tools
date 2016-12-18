c8bef5361dcbc381b363911ff7bd21d6156c46b8
okabé <mikepicard01@gmail.com>
2016-12-18 11:14:38 -0500

    Update proxyrequest.sh

--- utils/proxyrequest.sh
+++ utils/proxyrequest.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#author: mp
-#comment: simple command for doing DNS lookups over a socks proxy
-#         requires proxychains to be installed
+
 usage () {
   echo "./proxydns RECORD domain.com"
   exit 1
