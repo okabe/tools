765eb4f39a561207dacf614a977a3c2e7dd2f98c
okabé <mikepicard01@gmail.com>
2016-12-18 15:28:16 -0500

    Update sharc.sh

--- transport/sharc.sh
+++ transport/sharc.sh
@@ -4,7 +4,7 @@ stub=$1
 host=$( echo $stub | cut -f1 -d: )
 port=$( echo $stub | cut -f2 -d: )
 
-echo "[+] The following weak algorithms are supported:"
+echo "[+] The following weak cipher algorithms are supported:"
 ssh -p $port -o BatchMode=yes -vv test@$host 2>&1 \
     | grep "kex_parse_kexinit" \
     | cut -f3 -d: \
